cc61ec11a21885894dcf30e309dcf7611d7583c4
True Ghiassi <true@ghiassitrio.co.uk>
2021-08-19 20:39:16 +0100

    Shifted cipher module into ciphers.

--- noise/ciphers.py
+++ noise/ciphers.py
@@ -1,8 +1,29 @@
 import abc
-
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
-from noise.cipher import Cipher
+
+class Cipher(metaclass=abc.ABCMeta):
+    def __init__(self):
+        self.cipher = None
+
+    @property
+    @abc.abstractmethod
+    def klass(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def encrypt(self, k, n, ad, plaintext):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def decrypt(self, k, n, ad, ciphertext):
+        raise NotImplementedError
+
+    def rekey(self, k):
+        return self.encrypt(k, MAX_NONCE, b'', b'\x00' * 32)[:32]
+
+    def initialize(self, key):
+        self.cipher = self.klass(key)
 
 
 class CryptographyCipher(Cipher, metaclass=abc.ABCMeta):
