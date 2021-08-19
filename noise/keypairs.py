5b1996459944e2e9aa8ac97502c138db69d58b76
True Ghiassi <true@ghiassitrio.co.uk>
2021-08-19 20:43:05 +0100

    Got rid of keypair module.

--- noise/keypairs.py
+++ noise/keypairs.py
@@ -2,7 +2,24 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import x25519
 
 from noise.exceptions import NoiseValueError
-from noise.keypair import KeyPair
+import abc
+
+
+class KeyPair(metaclass=abc.ABCMeta):
+    def __init__(self, private=None, public=None, public_bytes=None):
+        self.private = private
+        self.public = public
+        self.public_bytes = public_bytes
+
+    @classmethod
+    @abc.abstractmethod
+    def from_private_bytes(cls, private_bytes):
+        raise NotImplementedError
+
+    @classmethod
+    @abc.abstractmethod
+    def from_public_bytes(cls, public_bytes):
+        raise NotImplementedError
 
 
 class KeyPair25519(KeyPair):
