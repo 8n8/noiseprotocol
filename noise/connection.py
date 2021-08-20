db437ce155f12e218688c7a6660e3952da1a8999
True Ghiassi <true@ghiassitrio.co.uk>
2021-08-20 21:17:28 +0100

    Shifted keypairs module into connection.

--- noise/connection.py
+++ noise/connection.py
@@ -7,7 +7,6 @@ from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 from noise.constants import MAX_MESSAGE_LEN
 from noise.exceptions import NoiseHandshakeError, NoiseInvalidMessage
 from cryptography.hazmat.primitives.asymmetric import x25519
-from noise.keypairs import KeyPair25519
 from noise.patterns import Pattern
 from noise.constants import (
     TOKEN_S,
@@ -25,9 +24,57 @@ from typing import Union
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.hmac import HMAC
+from cryptography.hazmat.primitives import serialization
 
 
 cryptography_backend = default_backend()
+
+
+class KeyPair(metaclass=abc.ABCMeta):
+    def __init__(self, private=None, public=None, public_bytes=None):
+        self.private = private
+        self.public = public
+        self.public_bytes = public_bytes
+
+    @classmethod
+    @abc.abstractmethod
+    def from_private_bytes(cls, private_bytes):
+        raise NotImplementedError
+
+    @classmethod
+    @abc.abstractmethod
+    def from_public_bytes(cls, public_bytes):
+        raise NotImplementedError
+
+
+class KeyPair25519(KeyPair):
+    @classmethod
+    def from_private_bytes(cls, private_bytes):
+        if len(private_bytes) != 32:
+            raise NoiseValueError("Invalid length of private_bytes! Should be 32")
+        private = x25519.X25519PrivateKey.from_private_bytes(private_bytes)
+        public = private.public_key()
+        return cls(
+            private=private,
+            public=public,
+            public_bytes=public.public_bytes(
+                encoding=serialization.Encoding.Raw,
+                format=serialization.PublicFormat.Raw,
+            ),
+        )
+
+    @classmethod
+    def from_public_bytes(cls, public_bytes):
+        if len(public_bytes) != 32:
+            raise NoiseValueError("Invalid length of public_bytes! Should be 32")
+        public = x25519.X25519PublicKey.from_public_bytes(public_bytes)
+        return cls(
+            public=public,
+            public_bytes=public.public_bytes(
+                encoding=serialization.Encoding.Raw,
+                format=serialization.PublicFormat.Raw,
+            ),
+        )
 
 
 class Hash(metaclass=abc.ABCMeta):
