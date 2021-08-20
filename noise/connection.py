cb6bbc315388fb8edd0a0cd1704f60f1b35fd433
True Ghiassi <true@ghiassitrio.co.uk>
2021-08-20 20:37:46 +0100

    Merged ciphers module into connections.

--- noise/connection.py
+++ noise/connection.py
@@ -1,11 +1,12 @@
 from enum import Enum
 
 from cryptography.exceptions import InvalidTag
+import abc
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
 from noise.constants import MAX_MESSAGE_LEN
 from noise.exceptions import NoiseHandshakeError, NoiseInvalidMessage
 from noise.diffie_hellmans import ED25519
-from noise.ciphers import ChaCha20Cipher
 from noise.hashes import BLAKE2sHash
 from noise.keypairs import KeyPair25519
 from noise.hashes import hmac_hash
@@ -23,6 +24,55 @@ from functools import partial
 from .constants import Empty
 import warnings
 from typing import Union
+
+
+class Cipher(metaclass=abc.ABCMeta):
+    def __init__(self):
+        self.cipher = None
+
+    @property
+    @abc.abstractmethod
+    def klass(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def encrypt(self, k, n, ad, plaintext):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def decrypt(self, k, n, ad, ciphertext):
+        raise NotImplementedError
+
+    def rekey(self, k):
+        return self.encrypt(k, MAX_NONCE, b"", b"\x00" * 32)[:32]
+
+    def initialize(self, key):
+        self.cipher = self.klass(key)
+
+
+class CryptographyCipher(Cipher, metaclass=abc.ABCMeta):
+    def encrypt(self, k, n, ad, plaintext):
+        return self.cipher.encrypt(
+            nonce=self.format_nonce(n), data=plaintext, associated_data=ad
+        )
+
+    def decrypt(self, k, n, ad, ciphertext):
+        return self.cipher.decrypt(
+            nonce=self.format_nonce(n), data=ciphertext, associated_data=ad
+        )
+
+    @abc.abstractmethod
+    def format_nonce(self, n):
+        raise NotImplementedError
+
+
+class ChaCha20Cipher(CryptographyCipher):
+    @property
+    def klass(self):
+        return ChaCha20Poly1305
+
+    def format_nonce(self, n):
+        return b"\x00\x00\x00\x00" + n.to_bytes(length=8, byteorder="little")
 
 
 class CipherState(object):
