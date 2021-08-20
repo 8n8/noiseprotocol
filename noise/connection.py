774ec1b8385b9d41d62b97f66ce277eb4e3f3263
True Ghiassi <true@ghiassitrio.co.uk>
2021-08-20 20:39:56 +0100

    Merged diffie_hellmans module into connections.

--- noise/connection.py
+++ noise/connection.py
@@ -6,8 +6,8 @@ from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
 from noise.constants import MAX_MESSAGE_LEN
 from noise.exceptions import NoiseHandshakeError, NoiseInvalidMessage
-from noise.diffie_hellmans import ED25519
 from noise.hashes import BLAKE2sHash
+from cryptography.hazmat.primitives.asymmetric import x25519
 from noise.keypairs import KeyPair25519
 from noise.hashes import hmac_hash
 from noise.patterns import Pattern
@@ -24,6 +24,56 @@ from functools import partial
 from .constants import Empty
 import warnings
 from typing import Union
+
+
+class DH(metaclass=abc.ABCMeta):
+    @property
+    @abc.abstractmethod
+    def klass(self):
+        raise NotImplementedError
+
+    @property
+    @abc.abstractmethod
+    def dhlen(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def generate_keypair(self) -> "KeyPair":
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def dh(self, private_key, public_key) -> bytes:
+        raise NotImplementedError
+
+
+class ED25519(DH):
+    @property
+    def klass(self):
+        return KeyPair25519
+
+    @property
+    def dhlen(self):
+        return 32
+
+    def generate_keypair(self) -> "KeyPair":
+        private_key = x25519.X25519PrivateKey.generate()
+        public_key = private_key.public_key()
+        return KeyPair25519(
+            private_key,
+            public_key,
+            public_key.public_bytes(
+                serialization.Encoding.Raw, serialization.PublicFormat.Raw
+            ),
+        )
+
+    def dh(self, private_key, public_key) -> bytes:
+        if not isinstance(private_key, x25519.X25519PrivateKey) or not isinstance(
+            public_key, x25519.X25519PublicKey
+        ):
+            raise NoiseValueError(
+                "Invalid keys! Must be x25519.X25519PrivateKey and x25519.X25519PublicKey instances"
+            )
+        return private_key.exchange(public_key)
 
 
 class Cipher(metaclass=abc.ABCMeta):
