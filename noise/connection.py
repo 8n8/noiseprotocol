be7cf53dacabe7542697abd7c5efe6e5ef3ecac8
True Ghiassi <true@ghiassitrio.co.uk>
2021-08-21 15:06:27 +0100

    Simplified hkdf function in NoiseProtocol by hard-coding options that never change.

--- noise/connection.py
+++ noise/connection.py
@@ -596,22 +596,22 @@ class HandshakeState(object):
         return keypair
 
 
-def hkdf(chaining_key, input_key_material, num_outputs, hmac_hash_fn):
+def hkdf(chaining_key, input_key_material, num_outputs):
     # Sets temp_key = HMAC-HASH(chaining_key, input_key_material).
-    temp_key = hmac_hash_fn(chaining_key, input_key_material)
+    temp_key = hmac_hash(chaining_key, input_key_material)
 
     # Sets output1 = HMAC-HASH(temp_key, byte(0x01)).
-    output1 = hmac_hash_fn(temp_key, b"\x01")
+    output1 = hmac_hash(temp_key, b"\x01")
 
     # Sets output2 = HMAC-HASH(temp_key, output1 || byte(0x02)).
-    output2 = hmac_hash_fn(temp_key, output1 + b"\x02")
+    output2 = hmac_hash(temp_key, output1 + b"\x02")
 
     # If num_outputs == 2 then returns the pair (output1, output2).
     if num_outputs == 2:
         return output1, output2
 
     # Sets output3 = HMAC-HASH(temp_key, output2 || byte(0x03)).
-    output3 = hmac_hash_fn(temp_key, output2 + b"\x03")
+    output3 = hmac_hash(temp_key, output2 + b"\x03")
 
     # Returns the triple (output1, output2, output3).
     return output1, output2, output3
@@ -652,7 +652,7 @@ class NoiseProtocol(object):
         self.dh_fn = ED25519()
         self.hash_fn = BLAKE2sHash()
         self.hmac = hmac_hash
-        self.hkdf = partial(self.backend.hkdf, hmac_hash_fn=self.hmac)
+        self.hkdf = hkdf
 
         # Initialized where needed
         self.cipher_class = ChaCha20Cipher
