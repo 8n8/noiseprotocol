ab5bbcddbe6178997a169603dadd3dfd5df63fd7
True Ghiassi <true@ghiassitrio.co.uk>
2021-08-25 11:36:35 +0100

    Got rid of unused cipher_state_decrypt field from NoiseProtocol.

--- noise/connection.py
+++ noise/connection.py
@@ -456,7 +456,6 @@ class NoiseProtocol(object):
     def __init__(self):
         # A valid Pattern instance (see Section 7 of specification (rev 32))
         # Preinitialized
-        self.cipher_state_decrypt = None
 
         self.keypairs = {"s": None, "e": None, "rs": None, "re": None}
 
